945a7efce860ce2188ea8e2e8f52f82ac4f3e119
Claudiu <claudiu.ardelean@plentydevelopment.com>
2023-05-12 13:02:46 +0300

    test

--- src/Controllers/CheckoutController.php
+++ src/Controllers/CheckoutController.php
@@ -655,15 +655,14 @@ class CheckoutController extends Controller
         $this->logger->debug('Controller.Success', $this->request->all());
         $transactionBasketId = $this->request->get('transactionBasketId');
 
+        /** @var BasketRepositoryContract $bascketRepo */
+        $bascketRepo = pluginApp(BasketRepositoryContract::class);
+        $basketContent = $bascketRepo->load()->toArray();
+        $this->logger->debug('Controller.Success', ['bascket' => $basketContent]);
+
         if (strlen($transactionBasketId)) {
             $storedBasketId = $paymentCache->getActiveBasketId();
             if ($storedBasketId === null) {
-
-                /** @var BasketRepositoryContract $bascketRepo */
-                $bascketRepo = pluginApp(BasketRepositoryContract::class);
-                $basketContent = $bascketRepo->load()->toArray();
-                $this->logger->debug('Controller.Success', ['bascket' => $basketContent]);
-
                 return $this->response->redirectTo('confirmation');
             }
             if ($storedBasketId != $transactionBasketId) {
