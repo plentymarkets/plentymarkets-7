baefe3116183dc22b17ba741c465ad5c95c6791c
claudiu <claudiu.ardelean@plentymarkets.com>
2023-05-15 20:51:53 +0300

    test

--- src/Services/PaymentCreation.php
+++ src/Services/PaymentCreation.php
@@ -121,7 +121,7 @@ class PaymentCreation
 
         $payment->mopId = (int) $mopId;
         $payment->transactionType = Payment::TRANSACTION_TYPE_BOOKED_POSTING;
-        $payment->status =  $response->getRedirecturl() !== '' ? Payment::STATUS_AWAITING_APPROVAL :  Payment::STATUS_APPROVED;
+        $payment->status =  $response->getRedirecturl() !== '' ? Payment::STATUS_AWAITING_APPROVAL :  Payment::STATUS_DUPLICATED;
 
         $payment->currency = $paymentData['basket']['currency'];
 
