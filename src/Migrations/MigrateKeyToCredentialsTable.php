a29fae6212814464c6b86a1a716ec95cafbd008b
Nopcea Ligia <ligia.nopcea@plentymarkets.com>
2024-03-13 17:03:49 +0200

    change migration

--- src/Migrations/MigrateKeyToCredentialsTable.php
+++ src/Migrations/MigrateKeyToCredentialsTable.php
@@ -4,25 +4,45 @@ namespace Payone\Migrations;
 
 use Payone\Models\Settings;
 use Payone\Repositories\LoginRepository;
-use Plenty\Modules\Market\Credentials\Models\Credentials;
+use Payone\Services\SettingsService;
 use Plenty\Modules\Plugin\DataBase\Contracts\DataBase;
+use Plenty\Plugin\CachingRepository;
+use Plenty\Plugin\Log\Loggable;
 
 class MigrateKeyToCredentialsTable
 {
+    use Loggable;
+
     public function run()
     {
+        $cachingRepository = pluginApp(CachingRepository::class);
         $database = pluginApp(DataBase::class);
 
         /** @var Settings[] $allSettings */
         $allSettings = $database->query(Settings::class)->get();
+        $this->getLogger(__METHOD__)->debug('Payone::General.objectData', $allSettings);
         /** @var LoginRepository $loginRepository */
         $loginRepository = pluginApp(LoginRepository::class);
 
         foreach ($allSettings as $setting) {
-            $newLogin = $loginRepository->create($setting->value);
+            try {
+                $credentialsSettings = $loginRepository->create($setting->value);
+                $cachingRepository->forget(
+                    SettingsService::CACHING_KEY_SETTINGS . '_' . $setting->clientId . '_' . $setting->pluginSetId
+                );
+                unset($credentialsSettings->data['key'], $credentialsSettings->data['PAYONE_PAYONE_INVOICE_SECURE']);
+                $cachingRepository->add(
+                    SettingsService::CACHING_KEY_SETTINGS . '_' . $setting->clientId . '_' . $setting->pluginSetId,
+                    $credentialsSettings,
+                    1440
+                ); //One day
+
                 unset($setting->value);
-                $setting->value['loginId'] = $newLogin->id;
+                $setting->value['loginId'] = $credentialsSettings->id;
                 $setting->save();
+            } catch (\Exception $ex) {
+                $this->getLogger(__METHOD__)->debug('Payone::General.objectData', $ex->getTrace());
+            }
         }
     }
 }
