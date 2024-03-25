0c3a48b0f4287774767f459f9f1b634e236885f9
Nopcea Ligia <ligia.nopcea@plentymarkets.com>
2024-03-25 11:41:23 +0200

    change Settings model

--- src/Models/Settings.php
+++ src/Models/Settings.php
@@ -48,9 +48,31 @@ class Settings extends Model
         $this->createdAt = (string)Carbon::now();
 
         $this->value = [
-            'loginId' => $data['loginId'] ?? ''
+            'loginId' => $data['loginId'] ?? '',
+            'mid' => $data['mid'],
+            'portalId' => $data['portalId'],
+            'aid' => $data['aid'],
+            'mode' => $data['mode'],
+            'authType' => $data['authType'],
+            'userId' => $data['userId'],
+            'PAYONE_PAYONE_INVOICE' => $data['PAYONE_PAYONE_INVOICE'],
+            'PAYONE_PAYONE_PAYDIREKT' => $data['PAYONE_PAYONE_PAYDIREKT'],
+            'PAYONE_PAYONE_PAYOLUTION_INSTALLMENT' => $data['PAYONE_PAYONE_PAYOLUTION_INSTALLMENT'],
+            'PAYONE_PAYONE_PAY_PAL' => $data['PAYONE_PAYONE_PAY_PAL'],
+            'PAYONE_PAYONE_RATEPAY_INSTALLMENT' => $data['PAYONE_PAYONE_RATEPAY_INSTALLMENT'],
+            'PAYONE_PAYONE_SOFORT' => $data['PAYONE_PAYONE_SOFORT'],
+            'PAYONE_PAYONE_CASH_ON_DELIVERY' => $data['PAYONE_PAYONE_CASH_ON_DELIVERY'],
+            'PAYONE_PAYONE_PRE_PAYMENT' => $data['PAYONE_PAYONE_PRE_PAYMENT'],
+            'PAYONE_PAYONE_CREDIT_CARD' => $data['PAYONE_PAYONE_CREDIT_CARD'],
+            'PAYONE_PAYONE_DIRECT_DEBIT' => $data['PAYONE_PAYONE_DIRECT_DEBIT'],
+            'PAYONE_PAYONE_INVOICE_SECURE' => $data['PAYONE_PAYONE_INVOICE_SECURE'],
+            'PAYONE_PAYONE_KLARNA_DIRECT_BANK' => $data['PAYONE_PAYONE_KLARNA_DIRECT_BANK'],
+            'PAYONE_PAYONE_KLARNA_DIRECT_DEBIT' => $data['PAYONE_PAYONE_KLARNA_DIRECT_DEBIT'],
+            'PAYONE_PAYONE_KLARNA_INSTALLMENTS' => $data['PAYONE_PAYONE_KLARNA_INSTALLMENTS'],
+            'PAYONE_PAYONE_KLARNA_INVOICE' => $data['PAYONE_PAYONE_KLARNA_INVOICE']
         ];
 
+        unset($this->value['PAYONE_PAYONE_INVOICE_SECURE']['key']);
         return $this->save();
     }
 
@@ -76,6 +98,76 @@ class Settings extends Model
         if (isset($data['loginId'])) {
             $this->value['loginId'] = $data['loginId'];
         }
+        if (isset($data['mid'])) {
+            $this->value['mid'] = $data['mid'];
+        }
+        if (isset($data['portalId'])) {
+            $this->value['portalId'] = $data['portalId'];
+        }
+        if (isset($data['aid'])) {
+            $this->value['aid'] = $data['aid'];
+        }
+        if (isset($data['mode'])) {
+            $this->value['mode'] = $data['mode'];
+        }
+        if (isset($data['authType'])) {
+            $this->value['authType'] = $data['authType'];
+        }
+        if (isset($data['userId'])) {
+            $this->value['userId'] = $data['userId'];
+        }
+        if (isset($data['PAYONE_PAYONE_INVOICE'])) {
+            $this->value['PAYONE_PAYONE_INVOICE'] = $data['PAYONE_PAYONE_INVOICE'];
+        }
+        if (isset($data['PAYONE_PAYONE_PAYDIREKT'])) {
+            $this->value['PAYONE_PAYONE_PAYDIREKT'] = $data['PAYONE_PAYONE_PAYDIREKT'];
+        }
+        if (isset($data['PAYONE_PAYONE_PAYOLUTION_INSTALLMENT'])) {
+            $this->value['PAYONE_PAYONE_PAYOLUTION_INSTALLMENT'] = $data['PAYONE_PAYONE_PAYOLUTION_INSTALLMENT'];
+        }
+        if (isset($data['PAYONE_PAYONE_PAY_PAL'])) {
+            $this->value['PAYONE_PAYONE_PAY_PAL'] = $data['PAYONE_PAYONE_PAY_PAL'];
+        }
+        if (isset($data['PAYONE_PAYONE_RATEPAY_INSTALLMENT'])) {
+            $this->value['PAYONE_PAYONE_RATEPAY_INSTALLMENT'] = $data['PAYONE_PAYONE_RATEPAY_INSTALLMENT'];
+        }
+        if (isset($data['PAYONE_PAYONE_SOFORT'])) {
+            $this->value['PAYONE_PAYONE_SOFORT'] = $data['PAYONE_PAYONE_SOFORT'];
+        }
+        if (isset($data['PAYONE_PAYONE_CASH_ON_DELIVERY'])) {
+            $this->value['PAYONE_PAYONE_CASH_ON_DELIVERY'] = $data['PAYONE_PAYONE_CASH_ON_DELIVERY'];
+        }
+        if (isset($data['PAYONE_PAYONE_PRE_PAYMENT'])) {
+            $this->value['PAYONE_PAYONE_PRE_PAYMENT'] = $data['PAYONE_PAYONE_PRE_PAYMENT'];
+        }
+        if (isset($data['PAYONE_PAYONE_CREDIT_CARD'])) {
+            $this->value['PAYONE_PAYONE_CREDIT_CARD'] = $data['PAYONE_PAYONE_CREDIT_CARD'];
+        }
+        if (isset($data['PAYONE_PAYONE_DIRECT_DEBIT'])) {
+            $this->value['PAYONE_PAYONE_DIRECT_DEBIT'] = $data['PAYONE_PAYONE_DIRECT_DEBIT'];
+        }
+        if (isset($data['PAYONE_PAYONE_INVOICE_SECURE'])) {
+            $this->value['PAYONE_PAYONE_INVOICE_SECURE']['portalId'] = $data['PAYONE_PAYONE_INVOICE_SECURE']['portalId'];
+        }
+        if (isset($data['PAYONE_PAYONE_AMAZON_PAY'])) {
+            $this->value['PAYONE_PAYONE_AMAZON_PAY'] = $data['PAYONE_PAYONE_AMAZON_PAY'];
+        }
+        if (isset($data['PAYONE_PAYONE_KLARNA_DIRECT_BANK'])) {
+            $this->value['PAYONE_PAYONE_KLARNA_DIRECT_BANK'] = $data['PAYONE_PAYONE_KLARNA_DIRECT_BANK'];
+        }
+        if (isset($data['PAYONE_PAYONE_KLARNA_DIRECT_DEBIT'])) {
+            $this->value['PAYONE_PAYONE_KLARNA_DIRECT_DEBIT'] = $data['PAYONE_PAYONE_KLARNA_DIRECT_DEBIT'];
+        }
+        if (isset($data['PAYONE_PAYONE_KLARNA_INSTALLMENTS'])) {
+            $this->value['PAYONE_PAYONE_KLARNA_INSTALLMENTS'] = $data['PAYONE_PAYONE_KLARNA_INSTALLMENTS'];
+        }
+        if (isset($data['PAYONE_PAYONE_KLARNA_INVOICE'])) {
+            $this->value['PAYONE_PAYONE_KLARNA_INVOICE'] = $data['PAYONE_PAYONE_KLARNA_INVOICE'];
+        }
+        if (isset($data['payoneMethods'])) {
+            $this->value['payoneMethods'] = $data['payoneMethods'];
+        }
+
         return $this->save();
     }
 
